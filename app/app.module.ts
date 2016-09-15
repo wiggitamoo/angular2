ed19ab28076fcd7c1f8d3e71adf83d500f7cb0dd
Andre Henriques <andrec.henriques@gmail.com>
2016-09-15 10:52:18 -0400

    Step 2

---
+++ app/app.module.ts
@@ -0,0 +1,8 @@
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core/src/metadata/ng_module";
+
+@NgModule({
+    imports: [BrowserModule]
+})
+export class AppModule {
+}
